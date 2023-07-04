f42ff4c65c861ffe74383d31f64308ded387c234
FrancisEugenio| BSCPE 1-5 <128889785+FrancisEugenio28@users.noreply.github.com>
2023-07-04 20:26:21 +0800

    Done for the Speed hoping it works properly

--- Fan.py
+++ Fan.py
@@ -9,6 +9,23 @@ class Fan:
         self.__power = power
 # Create def in all values
     # Speed
+    def set_speed(self):
+        while True:
+            try:
+                speed_input = int(input('Set the speed of the fan (1 for Slow, 2 for Medium, 3 for Fast): '))
+                if speed_input == 1:
+                    self.__speed = 'Slow'
+                    break
+                elif speed_input == 2:
+                    self.__speed = 'Medium'
+                    break
+                elif speed_input == 3:
+                    self.__speed = 'Fast'
+                    break
+                else:
+                    print('Invalid input, please try again.')
+            except ValueError:
+                print('Invalid input, please enter a number.')
     # Radius
     # Color 
     # Power
