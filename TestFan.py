a0a407236744fb5074d85911373ac6a1fbcb1f38
FrancisEugenio| BSCPE 1-5 <128889785+FrancisEugenio28@users.noreply.github.com>
2023-07-04 23:01:14 +0800

    trying to call all the functions for fan1

--- TestFan.py
+++ TestFan.py
@@ -1,6 +1,13 @@
 # import our code 
+from Fan import Fan
 # create 2 fan for testing
+fan1 = Fan()
+fan2 = Fan()
 # call all the functions in fan1
-    # print the output
+fan1.set_speed()
+fan1.set_radius()
+fan1.set_color()
+fan1.set_power()
+# print the output
 # call all the functions in fan2
-    # print the output
+# print the output
