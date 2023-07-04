dcf23261b28ad2a731f992b011ed37edf64f48c9
FrancisEugenio| BSCPE 1-5 <128889785+FrancisEugenio28@users.noreply.github.com>
2023-07-04 23:06:37 +0800

    successful calling all the variables

--- TestFan.py
+++ TestFan.py
@@ -9,5 +9,6 @@ fan1.set_radius()
 fan1.set_color()
 fan1.set_power()
 # print the output
+print('\n[Fan number 1] \nSpeed: ', fan1.get_speed(), '\nRadius: ', fan1.get_radius(), '\nColor: ', fan1.get_color(), '\nPower: ', fan1.get_power())
 # call all the functions in fan2
 # print the output
