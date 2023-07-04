7ec847faf526ba59a4282e5625636b795df9546c
FrancisEugenio| BSCPE 1-5 <128889785+FrancisEugenio28@users.noreply.github.com>
2023-07-04 20:40:36 +0800

    did some editing

--- Fan.py
+++ Fan.py
@@ -12,37 +12,34 @@ class Fan:
     def set_speed(self):
         while True:
             try:
-                speed_input = int(input('Set the speed of the fan (1 for Slow, 2 for Medium, 3 for Fast): '))
-                if speed_input == 1:
+                self.__speed = int(input('Set the speed of the fan (1 for Slow, 2 for Medium, 3 for Fast): '))
+                if self.__speed == 1:
                     self.__speed = 'Slow'
                     break
-                elif speed_input == 2:
+                elif self.__speed == 2:
                     self.__speed = 'Medium'
                     break
-                elif speed_input == 3:
+                elif self.__speed == 3:
                     self.__speed = 'Fast'
                     break
                 else:
                     print('Invalid input, please try again.')
             except ValueError:
                 print('Invalid input, please enter a number.')
-        return self.__speed
     # Radius
     def set_radius(self):
         self.__radius = int(input('Set the radius of the fan: '))
-        return self.__radius
     # Color 
     def set_color(self):
         self.__color = str(input('Please describe the color of the Fan: '))
-        return self.__color
     # Power
     def set_power(self):
         while True:
             try:
-                power_input = int(input('(0 if you want to turn off the fan, 1 if you want to turn on the fan) | 1 | 0 |: '))
-                if power_input == 1:
+                self.__power = int(input('(0 if you want to turn off the fan, 1 if you want to turn on the fan) | 1 | 0 |: '))
+                if self.__power == 1:
                     self.__power = 'ON'
-                elif power_input == 0:
+                elif self.__power == 0:
                     self.__power = "OFF"
                     break
                 else:
