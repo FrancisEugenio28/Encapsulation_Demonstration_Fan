24ec4da1d12743e37e05c7784756a5a702aa459a
FrancisEugenio| BSCPE 1-5 <128889785+FrancisEugenio28@users.noreply.github.com>
2023-07-04 20:06:35 +0800

    Done creating a class and a constructor with the default values in it

--- Fan.py
+++ Fan.py
@@ -1,5 +1,12 @@
 # create a class (Fan)
+class Fan:
 # create a constructor contains the values needed
+    def __init__(self, speed = 'Slow', radius = 5, color = 'Blue', power = 'off'):
+        # create the variables applying encapsulation method
+        self.__speed = speed
+        self.__radius = radius
+        self.__color = color
+        self.__power = power
 # Create def in all values
     # Speed
     # Radius
