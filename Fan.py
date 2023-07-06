9715dd95fc32a446ce65f7b73a88f599670a7e85
FrancisEugenio| BSCPE 1-5 <128889785+FrancisEugenio28@users.noreply.github.com>
2023-07-07 04:14:05 +0800

    Do some changes

--- Fan.py
+++ Fan.py
@@ -10,48 +10,29 @@ class Fan:
 # Create def in all values (setters)
     # Speed
     def set_speed(self):
-        while True:
-            try:
-                self.__speed = int(input('Set the speed of the fan (1 for Slow, 2 for Medium, 3 for Fast): '))
-                if self.__speed == 1:
-                    self.__speed = 'Slow'
-                    break
-                elif self.__speed == 2:
-                    self.__speed = 'Medium'
-                    break
-                elif self.__speed == 3:
-                    self.__speed = 'Fast'
-                    break
-                else:
-                    print('Invalid input, please try again.')
-            except ValueError:
-                print('Invalid input, please enter a number.')
+        if self.__speed == 1:
+            self.__speed = 'Slow'
+        elif self.__speed == 2:
+            self.__speed = 'Medium'
+        elif self.__speed == 3:
+            self.__speed = 'Fast'
     # Radius
     def set_radius(self):
-        self.__radius = int(input('Set the radius of the fan: '))
+        return self.__radius
     # Color 
     def set_color(self):
-        self.__color = str(input('Please describe the color of the Fan: '))
+        return self.__color
     # Power
     def set_power(self):
-        while True:
-            try:
-                self.__power = int(input('(0 if you want to turn off the fan, 1 if you want to turn on the fan) | 1 | 0 |: '))
-                if self.__power == 1:
-                    self.__power = 'ON'
-                    break
-                elif self.__power == 0:
-                    self.__power = "OFF"
-                    break
-                else:
-                    print('Invalid input, please try again.')
-            except ValueError:
-                print('Invalid input, please enter a number.')
+        if self.__power == 1:
+            self.__power = 'ON'
+        elif self.__power == 0:
+            self.__power = "OFF"
 
 # Returning all the variables from the setters
     # speed
     def get_speed(self):
-        return str(self.__speed)
+        return self.__speed
     # radius
     def get_radius(self):
         return self.__radius
@@ -60,4 +41,4 @@ class Fan:
         return self.__color
     # power
     def get_power(self):
-        return str(self.__power)
+        return self.__power
